ac6c0414d8299be837303b30f5124cbdea24d3d8
unknown <jinalgajera.ap@gmail.com>
2020-05-04 11:39:36 +0530

    add image confirmation page

--- confirmbooking.php
+++ confirmbooking.php
@@ -5,7 +5,6 @@ include('includes/config.php');
 session_start();
 $db_handle = new DBController();
 
-	
 if(isset($_GET["booking_id"]))
 {
     $id = $_GET["booking_id"];
@@ -26,24 +25,15 @@ if(isset($_GET["booking_id"]))
     $car = mysqli_fetch_assoc($carName);
    
 }
-                
-    
-
-
 ?>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-<!DOCTYPE html>
-<html>
-<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<meta name="keywords" content="">
-<meta name="description" content="">
-<title>Car Rental Portal</title>
-<!--Bootstrap -->
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CimbClicks</title>
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
 <link rel="stylesheet" href="assets/css/style.css" type="text/css">
 <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
@@ -51,54 +41,27 @@ if(isset($_GET["booking_id"]))
 <link href="assets/css/slick.css" rel="stylesheet">
 <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
-</head>
-<body>
-
-<!-- Start Switcher -->
-<?php include('includes/colorswitcher.php');?>
-<!-- /Switcher -->  
-        
-<!--Header-->
-<?php include('includes/header.php');?>
-<!-- /Header --> 
-
-<h1 align="center">Your Booked Order Detail</h1>
-</body>
-</html>
-
-<html>
-<head><title>Cars</title>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
- 
-
 </head>
+
 <body>
+<img  align="middle" src="gambarclick.png"  alt="Cimb Clicks" style="width:100%;">
+    <!--<?php echo $_SESSION['book_fee'];
+    //echo $_SERVER["REQUEST_URI"];
+    echo "<br/>";
+    // qecho $_SESSION["return_url"];
+	$cancel =  $_SESSION["return_url"];
+    ?>-->
+<br />
+<br />
+<br />
 
-<form method="post" action="" enctype="multipart/form-data">  
-<div class="container">
-		
-		
-			
 
-
-<br>
-<br>
-<input type="hidden" class="form-control" name="booking_fee" value="<?php echo $row["rate"]; ?>">
-<table id="example" class="table table-striped table-bordered">
+<br />
+<h1 align="center">Your Booked Order Detail</h1>
+<table id="example" class="table table-striped table-bordered" style="margin-left:12px;width:98% !important;">
 
 	<thead>
 		<tr>		
@@ -129,8 +92,6 @@ if(isset($_GET["booking_id"]))
 </table>
 
 
-<br>
 
-
-</form>
-</body>							
+</body>
+</html>
